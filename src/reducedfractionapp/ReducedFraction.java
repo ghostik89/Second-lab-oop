32d2eb7e0ec6ec44ceae0240681f852f3b59f16f
Lady_Proger <facktartar@mail.com>
2020-09-17 00:03:31 +0400

    Add fields and constructor. Add create static methods for creating reduced fraction

--- src/reducedfractionapp/ReducedFraction.java
+++ src/reducedfractionapp/ReducedFraction.java
@@ -7,20 +7,46 @@ public class ReducedFraction {
     /* =========================== Свойства =============================== */
  
     /* ---------------------- Числитель и знаменатель --------------------- */
-    
+    private int _numerator = 1;
+    private int _denominator = 1;
     
     
     /* =========================== Операции ============================== */
 
     /* ---------------------------- Порождение ---------------------------- */
-    
+    private static int gcd(int a,int b) {
+        while (b !=0) {
+            int tmp = a%b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+    /*
+    * Конструктор класса
+    *
+    * */
+    private ReducedFraction(int numerator, int denominator){
+        int tempGCD = gcd(numerator, denominator);
+        this._numerator = numerator / tempGCD;
+        this._denominator = denominator / tempGCD;
+    }
+
+
     /** Создание дроби с указанием ее числителя и знаменателя.
     * 
-    */ 
+    */
+    public static ReducedFraction createReducedFraction(int numerator, int denominator){
+        return new ReducedFraction(numerator, denominator);
+    }
 
     /** Создание дроби на основе целого числа.
     * 
-    */ 
+    */
+    public static ReducedFraction createReducedFracionFromNumber(int number){
+        return new ReducedFraction(number, 1);
+    }
     
   
     /* --------------------- Арифметические операции ---------------------- */

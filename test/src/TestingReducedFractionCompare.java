3f600ee52f88e7d169f67bf7550c24eb7099fffa
Lady_Proger <facktartar@mail.com>
2020-09-21 17:27:00 +0400

    add compare tests

--- test/src/TestingReducedFractionCompare.java
+++ test/src/TestingReducedFractionCompare.java
@@ -1,2 +1,39 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import reducedfractionapp.ReducedFraction;
+
 public class TestingReducedFractionCompare {
+
+    @Test()
+    void lessCompare() {
+        ReducedFraction compared1 = ReducedFraction.createReducedFraction(2,4);
+        ReducedFraction compared2 = ReducedFraction.createReducedFraction(2,3);
+
+        Assert.assertTrue(compared1.compare(compared2) < 0);
+    }
+
+    @Test()
+    void moreCompare() {
+        ReducedFraction compared1 = ReducedFraction.createReducedFraction(2,3);
+        ReducedFraction compared2 = ReducedFraction.createReducedFraction(2,8);
+
+        Assert.assertTrue(compared1.compare(compared2) > 0);
+    }
+
+    @Test()
+    void equalCompare() {
+        ReducedFraction compared1 = ReducedFraction.createReducedFraction(2,3);
+        ReducedFraction compared2 = ReducedFraction.createReducedFraction(2,3);
+
+        Assert.assertEquals(0, compared1.compare(compared2));
+    }
+
+    @Test()
+    void negativeCompare() {
+        ReducedFraction compared1 = ReducedFraction.createReducedFraction(-2,3);
+        ReducedFraction compared2 = ReducedFraction.createReducedFraction(2,3);
+
+        Assert.assertTrue(compared1.compare(compared2) < 0);
+    }
+
 }
